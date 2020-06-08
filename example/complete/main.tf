38f60aace3bb5e6e735177d1e73dc635fe3079e5
Luke Devonshire <luke@MacBook-Pro.local>
2020-06-08 08:46:45 +0100

    Updated paths, standardised naming

--- example/complete/main.tf
+++ example/complete/main.tf
@@ -11,7 +11,7 @@ module "naming" {
   source = "git::https://github.com/Azure/terraform-azurerm-naming"
 }
 resource "azurerm_resource_group" "test_group" {
-  name     = "${module.naming.resource_group.slug}-${module.naming.storage_managed_encryption_key.slug}-min-test-${local.unique_name_stub}"
+  name     = "${module.naming.resource_group.slug}-${module.naming.storage_managed_encryption_key.slug}-max-test-${local.unique_name_stub}"
   location = "uksouth"
 }
 
